f369b772921a4a3ac5cecc07d50592ba32a0da96
Eduardo <edu.baldini27@gmail.com>
2021-07-08 13:32:09 -0300

    actual questions commit

--- src/style.py
+++ src/style.py
@@ -62,196 +62,147 @@ ScreenManager:
 <Question2Screen>:
     name: 'question2'
     MDLabel:
-        text:  'questao2'
+        text:  'Uma situação igual ou parecida a essa já aconteceu no passado? Avalie como foi de ótima a péssima:'
         halign: 'center'
          
 
     MDRectangleFlatButton:
         text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        pos_hint: {'center_x':0.1, 'center_y':0.2}
         on_press: root.manager.current = 'question3'
 
     MDRectangleFlatButton:
         text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'question3'
 
     MDRectangleFlatButton:
         text: 'Média'
-        pos_hint: {'center_x':0.50, 'center_y':0.2}
+        pos_hint: {'center_x':0.4, 'center_y':0.2}
         on_press: root.manager.current = 'question3'
 
     MDRectangleFlatButton:
         text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
+        pos_hint: {'center_x':0.575, 'center_y':0.2}
         on_press: root.manager.current = 'question3'
     
     MDRectangleFlatButton:
         text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
+        pos_hint: {'center_x':0.725, 'center_y':0.2}
+        on_press: root.manager.current = 'question3'
+
+    MDRectangleFlatButton:
+        text: 'Não se aplica'
+        pos_hint: {'center_x':0.9, 'center_y':0.2}
         on_press: root.manager.current = 'question3'
 
 <Question3Screen>:
     name: 'question3'
     MDLabel:
-        text:  'questao3'
+        text:  'Uma situação igual ou parecida a essa já te machucou no passado?'
         halign: 'center'
          
-
     MDRectangleFlatButton:
-        text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        text: 'Sim'
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'question4'
 
     MDRectangleFlatButton:
-        text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
-        on_press: root.manager.current = 'question4'
-
-    MDRectangleFlatButton:
-        text: 'Média'
+        text: 'Não se aplica'
         pos_hint: {'center_x':0.50, 'center_y':0.2}
         on_press: root.manager.current = 'question4'
 
     MDRectangleFlatButton:
-        text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
+        text: 'Não'
+        pos_hint: {'center_x':0.75, 'center_y':0.2}
         on_press: root.manager.current = 'question4'
-    
-    MDRectangleFlatButton:
-        text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
-        on_press: root.manager.current = 'question4'
-
 
 <Question4Screen>:
     name: 'question4'
     MDLabel:
-        text:  'questao 4'
+        text:  'Você terá outra oportunidade de fazer o que essa decisão implica num futuro próximo?'
         halign: 'center'
          
 
     MDRectangleFlatButton:
-        text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        text: 'Sim'
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'question5'
 
     MDRectangleFlatButton:
-        text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
-        on_press: root.manager.current = 'question5'
-
-    MDRectangleFlatButton:
-        text: 'Média'
+        text: 'Não se aplica'
         pos_hint: {'center_x':0.50, 'center_y':0.2}
         on_press: root.manager.current = 'question5'
 
     MDRectangleFlatButton:
-        text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
+        text: 'Não'
+        pos_hint: {'center_x':0.75, 'center_y':0.2}
         on_press: root.manager.current = 'question5'
-    
-    MDRectangleFlatButton:
-        text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
-        on_press: root.manager.current = 'question5'
-
 
 <Question5Screen>:
     name: 'question5'
     MDLabel:
-        text:  'questao 5'
+        text:  'Você sente que existe a possibilidade de você se arrepender caso não faça o que essa decisão implica?'
         halign: 'center'
          
 
     MDRectangleFlatButton:
-        text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        text: 'Sim'
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'question6'
 
     MDRectangleFlatButton:
-        text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
-        on_press: root.manager.current = 'question6'
-
-    MDRectangleFlatButton:
-        text: 'Média'
+        text: 'Não se aplica'
         pos_hint: {'center_x':0.50, 'center_y':0.2}
         on_press: root.manager.current = 'question6'
 
     MDRectangleFlatButton:
-        text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
+        text: 'Não'
+        pos_hint: {'center_x':0.75, 'center_y':0.2}
         on_press: root.manager.current = 'question6'
-    
-    MDRectangleFlatButton:
-        text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
-        on_press: root.manager.current = 'question6'
-
 
 <Question6Screen>:
     name: 'question6'
     MDLabel:
-        text:  'questao 6'
+        text:  'A situação envolve questões que podem te prejudicar ou prejudicar alguém que você ama?'
         halign: 'center'
          
 
     MDRectangleFlatButton:
-        text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        text: 'Sim'
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'question7'
 
     MDRectangleFlatButton:
-        text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
-        on_press: root.manager.current = 'question7'
-
-    MDRectangleFlatButton:
-        text: 'Média'
+        text: 'Não se aplica'
         pos_hint: {'center_x':0.50, 'center_y':0.2}
         on_press: root.manager.current = 'question7'
 
     MDRectangleFlatButton:
-        text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
-        on_press: root.manager.current = 'question7'
-    
-    MDRectangleFlatButton:
-        text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
+        text: 'Não'
+        pos_hint: {'center_x':0.75, 'center_y':0.2}
         on_press: root.manager.current = 'question7'
 
 <Question7Screen>:
     name: 'question7'
     MDLabel:
-        text:  'questao 7'
+        text:  'Pense na situação de maneira inversa, caso se aplique, você gostaria que a decisão afirmativa (positiva, sim) fosse tomada?'
         halign: 'center'
          
 
     MDRectangleFlatButton:
-        text: 'Ótima'
-        pos_hint: {'center_x':0.20, 'center_y':0.2}
+        text: 'Sim'
+        pos_hint: {'center_x':0.25, 'center_y':0.2}
         on_press: root.manager.current = 'rules2'
 
     MDRectangleFlatButton:
-        text: 'Boa'
-        pos_hint: {'center_x':0.35, 'center_y':0.2}
-        on_press: root.manager.current = 'rules2'
-
-    MDRectangleFlatButton:
-        text: 'Média'
+        text: 'Não se aplica'
         pos_hint: {'center_x':0.50, 'center_y':0.2}
         on_press: root.manager.current = 'rules2'
 
     MDRectangleFlatButton:
-        text: 'Ruim'
-        pos_hint: {'center_x':0.65, 'center_y':0.2}
-        on_press: root.manager.current = 'rules2'
-    
-    MDRectangleFlatButton:
-        text: 'Péssima'
-        pos_hint: {'center_x':0.80, 'center_y':0.2}
+        text: 'Não'
+        pos_hint: {'center_x':0.75, 'center_y':0.2}
         on_press: root.manager.current = 'rules2'
 
 <RulesScreen2>:
